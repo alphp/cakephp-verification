5af7fc946868cc0f3225874dd2349b5b99a22dfa
Fernando Herrero <ininflamable@gmail.com>
2026-06-28 03:08:41 +0200

    Automatic template loading from the app or plugin

--- src/Mailer/UserMailer.php
+++ src/Mailer/UserMailer.php
@@ -15,18 +15,28 @@
 		 * @var string
 		 */
 		public static string $name = 'User';
-		public function emailVerify (object $user, string $verifyUrl) : void {
-            $this->viewBuilder()
-                ->setPlugin('CakeVerification')
-                ->setTemplate('email_verify');
+
+        protected function setTemplate (string $name) : static {
+            $view = $this->viewBuilder();
+            $view->setTemplate($name);
+
+            foreach ($this->getMessage()->getBodyTypes() as $type) {
+                if (!is_file(ROOT . DS . 'templates' . DS . 'email' . DS . $type . DS . $name . '.php')) {
+                    $view->setPlugin('CakeVerification');
+                }
+            }
+
+            return $this;
+        }
+
+        public function emailVerify (object $user, string $verifyUrl) : void {
+            $this->setTemplate('email_verify');
             $this->setSubject(__('Confirm your email address'))
                 ->setViewVars(compact('user', 'verifyUrl'));
         }
 
         public function emailOtp (object $user, string $code) : void {
-            $this->viewBuilder()
-                ->setPlugin('CakeVerification')
-                ->setTemplate('email_otp');
+            $this->setTemplate('email_otp');
             $this->setSubject(__('Your login code'))
                 ->setViewVars(compact('user', 'code'));
         }
